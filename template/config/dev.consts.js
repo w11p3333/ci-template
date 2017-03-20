2a6362a34e33630c28e4fb0ba328641296dcc616
w11p3333 <applecatkay@gmail.com>
2017-03-20 22:46:28 +0800

    update config

---
+++ template/config/dev.consts.js
@@ -0,0 +1,13 @@
+/**
+ * Created by luliangxiao on 16/12/24.
+ */
+export default {
+  SERVICE_NAME: 'member', // 会员服务
+  DEV_DOMAIN: 'http://idg-luliangxiao.tunnel.nibaguai.com/', // TO FILL IN YOUR DOMAIN
+  DEV_USER_INFO: {
+    appkey: 'rjtwfgeqm2ankhx91wcp3hkeycujiutl',
+    channel: 1,
+    account_id: 'dev',
+    nickname: '开发者'
+  }
+}
