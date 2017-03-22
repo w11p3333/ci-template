c3372384830fadac2a20f30580e5ee84e8885f6d
w11p3333 <applecatkay@gmail.com>
2017-03-22 10:44:04 +0800

    update meta

--- meta.js
+++ meta.js
@@ -14,24 +14,16 @@ module.exports = {
       "required": true,
       "message": "Project name"
     },
-  "description": {
-    "type": "string",
-    "required": false,
-    "message": "Project description",
-    "default": "A Vue.js project"
-  },
-  "author": {
-    "type": "string",
-    "message": "Author"
-  },
-  "filters": {
-    ".eslintrc.js": "lint",
-    ".eslintignore": "lint",
-    "config/test.env.js": "unit || e2e",
-    "test/unit/**/*": "unit",
-    "build/webpack.test.conf.js": "unit",
-    "test/e2e/**/*": "e2e",
-    "src/router/**/*": "router"
+    "description": {
+      "type": "string",
+      "required": false,
+      "message": "Project description",
+      "default": "A Vue.js project"
+    },
+    "author": {
+      "type": "string",
+      "message": "Author"
+    }
   },
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
 };
