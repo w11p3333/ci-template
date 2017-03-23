7e623c1884dadbc729923adf7483f6d0df626daa
w11p3333 <applecatkay@gmail.com>
2017-03-23 09:24:50 +0800

    fix meta

--- meta.js
+++ meta.js
@@ -37,7 +37,7 @@ module.exports = {
     "config/test.env.js": "unit",
     "test/unit/**/*": "unit",
     "build/webpack.test.conf.js": "unit",
-    "src/views/example": "example"
+    "src/views/example.vue": "example"
   },
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
 };
