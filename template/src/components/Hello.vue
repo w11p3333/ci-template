a3b0cb3f211f7060b63ad364bd967e0ca08d943e
w11p3333 <applecatkay@gmail.com>
2017-03-23 11:54:25 +0800

    update hello

--- template/src/components/Hello.vue
+++ template/src/components/Hello.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="hello">
-    <div>
-      <img src="../assets/logo.png">
-      <h1>{{ title }}</h1>
-      <el-button @click="changeLanguage">{{ btnText }}</el-button>
-    </div>
+    <img src="../assets/logo.png">
+    <h1>{{ $t('hello.title') }}</h1>
+    <el-button @click="changeLanguage">{{ $t('hello.btn') }}</el-button>
     <ul>
       <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
